ca35dacedafc1bd2a039005a3542f4affc2c688e
yunhsuanchin <alicechin20181@gmail.com>
2020-10-22 17:05:24 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,16 +2,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
 require('./config/mongoose')
 
 // modules setting
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// routes
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
 
+// listen
 app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
 })
--- models/record.js
+++ models/record.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 const recordSchema = new Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
-  date: { type: Date, required: true, default: Date.now },
+  date: { type: String, required: true },
   amount: { type: Number, required: true }
 })
 
