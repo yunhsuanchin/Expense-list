2a81e00b7fa354907e9b2618b0ae052df304d2b3
yunhsuanchin <alicechin20181@gmail.com>
2020-11-09 16:25:00 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const router = express.Router()
 
-// require home.js & records.js
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/records', records)
+router.use('/users', users)
 
 module.exports = router
